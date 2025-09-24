736106e7a537b74200aa7e2d9a02b27ab807cdb6
chukwuebuka223 <patrickagwuncha952@gmail.com>
2025-09-24 21:22:40 +0100

    added css

--- first.js
+++ first.js
@@ -1,14 +1,13 @@
 let principalAmount = 1000;
 let annualInterest = 0.05;
-let timeInyear = 2;
+let timeInyear = 5;
 
 let interest = principalAmount * annualInterest * timeInyear
 console.log(interest)
 let totalAmmount = principalAmount + interest
 console.log(totalAmmount)
 
-let summary =  "after " + timeInyear + " years " + " at an " + interest + " rate of " + annualInterest + " %, the total amount will be N " + totalAmmount
-console.log(summary)
+let summary =`going forward to ${timeInyear} years at a ${interest} rate of ${annualInterest}%, the total amount will be N${totalAmmount}`
 let container = document.getElementById("container")
 container.innerText = summary
 
